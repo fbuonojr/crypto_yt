5e6013eaf0eb38498dad531a6fce0e6d12b770e1
fbuonojr <fbuonojr@fordham.edu>
2023-06-30 00:01:35 -0400

    install dependencies

--- crypto_react/src/App.js
+++ crypto_react/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
     </div>
   );
-}
+  }
 
 export default App;
